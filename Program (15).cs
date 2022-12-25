0e8888def7ef32f082363e19c204ef44c23802d1
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-12-25 14:46:29 +0200

    Add files via upload

---
+++ Program (15).cs
@@ -0,0 +1,11 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int units = number % 10;
+int tens = number % 100 / 10;
+int hundreds = number / 100;
+
+bool sumIsTwoDigit = (units + tens + hundreds).ToString().Length == 2;
+bool multIsThreeDigit = (units * tens * hundreds).ToString().Length == 3;
+
+Console.WriteLine($"Сумма цифр числа {(sumIsTwoDigit ? "" : "не ")}является двузначным числом.");
+Console.WriteLine($"Произведение цифр числа {(multIsThreeDigit ? "" : "не ")}является трехзначным числом.");
